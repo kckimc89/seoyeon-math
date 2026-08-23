47ffa82396f5e407a0b0911192543a05c6676108
kckimc89 <kckimc89@daum.net>
2026-08-23 11:07:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,7 @@
-const CACHE='seoyeon-math-v7.3';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='seoyeon-math-v7.3.2';
+self.addEventListener('install',e=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(
+ fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;})
+ .catch(()=>caches.match(e.request))
+));
